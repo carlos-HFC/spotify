8e3233618bdee8f9fc36b645aa8a840c5732c6bd
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-17 17:07:44 -0300

    liked songs component

---
+++ src/components/LikedSongs/index.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+import { SONGS } from "@/constants";
+
+export function LikedSongs() {
+  return (
+    <a href="#" className="h-[324px] max-w-[502px] flex-1 flex flex-col justify-end gap-9 p-6 bg-liked-songs rounded-lg relative group">
+      <div className="inline [&_*:nth-child(1)]:ml-0">
+        {SONGS.slice(0, 6).map(item => (
+          <div
+            className="text-xl ml-1 inline"
+            key={item.song}
+          >
+            {item.artist} <span className="text-white/60">{item.song} &bull;</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col relative">
+        <span className="font-bold text-4xl">Músicas Curtidas</span>
+        <p className="text-xl">1.517 músicas</p>
+      </div>
+
+      <Image
+        src="/assets/play-green.svg"
+        alt="Play"
+        width={94}
+        height={94}
+        className="absolute right-0 bottom-0 invisible group-hover:visible"
+      />
+    </a>
+  );
+}
