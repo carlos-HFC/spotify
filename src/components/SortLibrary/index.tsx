562b87f22434141ab8d4db2220c6c0c8d0b54f9e
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-18 16:00:14 -0300

    sort library component

---
+++ src/components/SortLibrary/index.tsx
@@ -0,0 +1,18 @@
+export function SortLibrary() {
+  return (
+    <div className="flex gap-3 *:rounded *:px-5 *:py-3 *:font-bold *:text-lg first:*:bg-white/30">
+      <button>
+        Playlists
+      </button>
+      <button>
+        Podcasts
+      </button>
+      <button>
+        Artistas
+      </button>
+      <button>
+        Álbuns
+      </button>
+    </div>
+  );
+}
