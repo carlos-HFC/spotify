d307c43930307cd19465ada3c9715a5185097d39
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-18 16:00:07 -0300

    search input component

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,8 @@
+export function SearchInput() {
+  return (
+    <input
+      className="rounded-full w-96 py-3 pl-16 text-lg text-grayscale-500 bg-bloom bg-[center_left_1rem] bg-no-repeat bg-blend-difference"
+      placeholder="Artistas, músicas ou podcasts"
+    />
+  );
+}
