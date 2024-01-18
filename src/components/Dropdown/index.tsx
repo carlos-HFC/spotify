9b974dce7fbb0444db4354b9ae84c170cbe05c1d
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-18 15:59:57 -0300

    dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { CONTEXT_MENU_HEADER } from "@/constants";
+import { cn } from "@/utils";
+
+export function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const closeDrop = (event: MouseEvent) => {
+      if ((event?.target as HTMLButtonElement)?.hasAttribute('aria-controls')) return;
+
+      setIsOpen(false);
+    };
+
+    document.body.addEventListener("click", closeDrop);
+    return () => document.body.removeEventListener("click", closeDrop);
+  }, []);
+
+  return (
+    <div className="relative">
+      <button
+        aria-controls="dropdown"
+        id="dropdown-list"
+        aria-label="Abrir e fechar menu"
+        aria-expanded={isOpen}
+        className="flex items-center p-1 pr-3 gap-3 bg-black/80 text-white rounded-full *:pointer-events-none"
+        onClick={() => setIsOpen(prev => !prev)}
+      >
+        <Image
+          src="/metallica.webp"
+          alt="Usuário logado"
+          width={32}
+          height={32}
+          className="rounded-full"
+        />
+        <p className="font-bold text-lg">Carlos Faustino</p>
+        <Image
+          src="/assets/arrow-down.svg"
+          alt="Seta para baixo"
+          width={16}
+          height={16}
+          className={cn("transition-transform", isOpen ? 'rotate-180' : '')}
+        />
+      </button>
+
+      <nav
+        id="dropdown"
+        aria-labelledby="dropdown-list"
+        className={cn("bg-grayscale-800 z-10 rounded w-64 absolute mt-2 top-full right-0 overflow-hidden transition-[max-height]", isOpen ? 'max-h-96' : 'max-h-0')}
+      >
+        <ul className="*:p-4 *:flex *:items-center *:justify-between *:text-lg *:rounded *:cursor-default *:select-none p-1.5 [&_*:last-of-type]:border-t [&_*:last-of-type]:border-grayscale-700 hover:*:bg-grayscale-700">
+          {CONTEXT_MENU_HEADER.map(item => (
+            <li key={item.label}>
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div >
+  );
+}
