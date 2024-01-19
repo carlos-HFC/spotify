696c337682d95b33a2bb66d3552787def711e5a4
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-19 17:38:17 -0300

    add function to convert minutes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,10 @@
 export function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
+
+export function convertMinutesToHours(value: number) {
+  const hours = Math.floor(value / 60);
+  const minutes = value % 60;
+
+  return `${hours}h ${minutes.toString().padStart(2, '0')}min`;
+}
