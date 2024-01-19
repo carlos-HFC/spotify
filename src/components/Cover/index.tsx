5cadb09bb22dcdb2841af3b257187bdc059e7449
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-19 17:38:05 -0300

    cover component

---
+++ src/components/Cover/index.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+import { convertMinutesToHours } from "@/utils";
+
+interface CoverProps {
+  title: string;
+  description?: string;
+  image: string;
+  authorImage: string;
+  author: string;
+  likes?: number;
+  qty: number;
+  time?: number;
+}
+
+export function Cover(props: CoverProps) {
+  return (
+    <div className="flex items-end gap-8 flex-1">
+      <div className="relative aspect-square w-[clamp(100px,30%,297px)]">
+        <Image
+          src={props.image}
+          alt={props.title}
+          fill
+          className="rounded shadow-xl shadow-black/25"
+        />
+      </div>
+
+      <div className="flex flex-col flex-1 gap-2">
+        <div>
+          <span className="block uppercase font-medium">Playlist pública</span>
+          <strong className="block font-black text-[clamp(24px,4vw,122px)]">{props.title}</strong>
+        </div>
+        {props.description && (
+          <div className="text-white/70 text-xl">{props.description}</div>
+        )}
+        <div className="flex items-center gap-2 text-lg flex-wrap">
+          <Image
+            src={props.authorImage}
+            alt={props.author}
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+
+          <a href="#" className="font-bold hover:underline">{props.author}</a>
+          <small className="text-xxs">&bull;</small>
+          {props.likes && (
+            <>
+              <span>{Intl.NumberFormat('pt-BR').format(props.likes)} likes</span>
+              <small className="text-xxs">&bull;</small>
+            </>
+          )}
+          <span className="text-white/70">
+            {props.qty} músicas{props.time && `, ${convertMinutesToHours(props.time)}`}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
