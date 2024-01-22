adbc9bfca46f4af0736e447c191629d461ca4d0a
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-22 14:44:12 -0300

    finished home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,63 @@
-"use client";
+import { Recent } from "@/components/Recent";
+import { SectionHome } from "@/components/SectionHome";
+import { Suggestion } from "@/components/Suggestion";
 
-import { MouseEvent } from "react";
-
-import { ContextMenu } from "@/components/ContextMenu";
-import { Music } from "@/components/Music";
-
-import { SONGS } from "@/constants";
-import { useContextMenu } from "@/hooks/useContextMenu";
+import { EPISODES, MADE_FOR_YOU, RECENTLY_PLAYED, YOUR_PLAYLISTS } from "@/constants";
 
 export default function Home() {
-  const contextMenu = useContextMenu();
-
-  function handleContextMenu(event: MouseEvent<HTMLDivElement | HTMLButtonElement>) {
-    event.preventDefault();
-    contextMenu.setClicked(true);
-
-    let coordX = event.pageX,
-      coordY = event.pageY;
-
-    const clientWidth = event.currentTarget.offsetWidth;
-    const clientHeight = document.documentElement.offsetHeight;
-
-    if (event.pageX - 20 > clientWidth) coordX -= 286;
-    if ((event.screenY + 115) + 430 > clientHeight) coordY -= 430;
-
-    contextMenu.setCoords({
-      x: coordX,
-      y: coordY
-    });
-  }
-
   return (
-    <div className="flex pb-28">
-      <div className="flex flex-col flex-1">
-        {SONGS.map((item, index) => (
-          <Music
+    <div className="flex flex-col gap-12 *:flex *:flex-col *:gap-6 bg-home px-10 py-24">
+      <section>
+        <h1 className="font-bold text-4xl">
+          Bom dia
+        </h1>
+
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(420px,1fr))] gap-x-8 gap-y-4">
+          {RECENTLY_PLAYED.slice(0, 6).map(item => (
+            <Recent
+              key={item.title}
+              image={item.image}
+              name={item.title}
+            />
+          ))}
+        </div>
+      </section>
+
+      <SectionHome title="Episódios para você">
+        {EPISODES.map(item => (
+          <Suggestion
             {...item}
-            key={item.song + index}
-            index={index + 1}
-            isPlaying={index === 1}
-            image="/metallica.webp"
-            onContextMenu={handleContextMenu}
+            key={item.title}
           />
         ))}
+      </SectionHome>
 
-        <ContextMenu
-          open={contextMenu.clicked}
-          top={contextMenu.coords.y}
-          left={contextMenu.coords.x}
-        />
-      </div>
+      <SectionHome title="Tocados recentemente">
+        {RECENTLY_PLAYED.map(item => (
+          <Suggestion
+            {...item}
+            key={item.title}
+          />
+        ))}
+      </SectionHome>
+
+      <SectionHome title="Feito para você">
+        {MADE_FOR_YOU.map(item => (
+          <Suggestion
+            {...item}
+            key={item.title}
+          />
+        ))}
+      </SectionHome>
+
+      <SectionHome title="Suas playlists">
+        {YOUR_PLAYLISTS.map(item => (
+          <Suggestion
+            {...item}
+            key={item.title}
+          />
+        ))}
+      </SectionHome>
     </div>
   );
 }
