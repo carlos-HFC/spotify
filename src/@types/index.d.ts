78e54f136cccec91acfb1f9f758602b0c6c60725
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-24 12:26:21 -0300

    global typing for playlists and tracks

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,18 @@
 type Genre = 'pop' | 'afro' | 'hip-hop' | 'podcasts' | 'made-for-you' | 'music' | 'live-event' | 'release' | 'brazil' | 'pagode' | 'mpb' | 'rock' | 'alternative' | 'blues' | 'punk' | 'classic' | 'instrumental' | 'jazz' | 'reggae' | 'tv' | 'games' | 'rb' | 'kpop' | 'latina' | 'soul' | 'folk' | 'country' | 'sertanejo';
+
+type Track = {
+  artist: string;
+  song: string;
+  album: string;
+  image: string;
+  time: string;
+  liked: boolean;
+  date: string;
+  description?: string;
+};
+
+type Playlist = {
+  title: string;
+  image: string;
+  description: string;
+};
