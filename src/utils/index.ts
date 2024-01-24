a7aebfd9fb5c67bd09c0f9f792650c8a54b40fb6
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-24 17:04:39 -0300

    order array dynamically

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,17 @@
 export function cn(...classes: Array<string | boolean | undefined>) {
   return classes.filter(Boolean).join(' ');
 }
+
+export function orderArray<T>(array: T[], key: keyof T, asc = false) {
+  return [...array]?.sort((a, b) => {
+    const varA = typeof a[key] === 'string' ? String(a[key]).toLowerCase() : a[key];
+    const varB = typeof b[key] === 'string' ? String(b[key]).toLowerCase() : b[key];
+
+    let order = 0;
+
+    if (varA > varB) order = 1;
+    else if (varA < varB) order = -1;
+
+    return asc ? order * -1 : order;
+  });
+}
