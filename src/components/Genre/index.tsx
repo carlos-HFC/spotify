4771b0e4f72f93fa50c8c738769d4fec5de33f97
Carlos Faustino <chfcchfc96@gmail.com>
2024-01-29 10:59:53 -0300

    create variant var

--- src/components/Genre/index.tsx
+++ src/components/Genre/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-import { VARIANT } from "@/constants";
 import { cn } from "@/utils";
 
 interface GenreProps {
@@ -11,10 +10,41 @@ interface GenreProps {
 }
 
 export function Genre(props: GenreProps) {
+  const VARIANT = {
+    pop: 'bg-genre-pop',
+    afro: 'bg-genre-afro',
+    'hip-hop': 'bg-genre-hip-hop',
+    podcasts: 'bg-genre-podcasts',
+    'made-for-you': 'bg-genre-made-for-you',
+    music: 'bg-genre-music',
+    'live-event': 'bg-genre-live-event',
+    release: 'bg-genre-release',
+    brazil: 'bg-genre-brazil',
+    pagode: 'bg-genre-pagode',
+    mpb: 'bg-genre-mpb',
+    rock: 'bg-genre-rock',
+    alternative: 'bg-genre-alternative',
+    blues: 'bg-genre-blues',
+    punk: 'bg-genre-punk',
+    classic: 'bg-genre-classic',
+    instrumental: 'bg-genre-instrumental',
+    jazz: 'bg-genre-jazz',
+    reggae: 'bg-genre-reggae',
+    tv: 'bg-genre-tv',
+    games: 'bg-genre-games',
+    rb: 'bg-genre-rb',
+    kpop: 'bg-genre-kpop',
+    latina: 'bg-genre-latina',
+    soul: 'bg-genre-soul',
+    folk: 'bg-genre-folk',
+    country: 'bg-genre-country',
+    sertanejo: 'bg-genre-sertanejo',
+  };
+
   return (
     <a
       href="#"
-      className={cn("overflow-hidden rounded-xl flex bg-blue-200 relative p-5", props.top ? "flex-1 h-64" : 'size-60', VARIANT[props.variant].background)}
+      className={cn("overflow-hidden rounded-xl flex relative max-w-[480px] p-5", props.top ? "flex-1 h-64" : 'size-60', VARIANT[props.variant])}
     >
       <div className={cn("font-bold", props.top ? "text-5xl tracking-[-2.6px]" : "text-3xl tracking-[-.9px]")}>
         {props.genre}
@@ -28,6 +58,6 @@ export function Genre(props: GenreProps) {
         loading="lazy"
         className="rotate-[25deg] shadow-black/15 shadow-[-2px_2px_6px_0] absolute -right-6 -bottom-2"
       />
-    </a >
+    </a>
   );
 }
